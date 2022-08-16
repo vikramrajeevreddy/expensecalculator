15338a0dbd016c5431fea474ffc5519c190fffb1
vikramrajeevreddy <rajivreddy.reddy20@gmail.com>
2022-08-16 17:45:48 -0400

    Updates

---
+++ precache-manifest.21e571d934873ec80d478c0fe066167a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4420d17628b8a9cce34cd0202cd64687",
+    "url": "/expensecalculator/index.html"
+  },
+  {
+    "revision": "3813435a184127f1a509",
+    "url": "/expensecalculator/static/css/main.4ccf6d8d.chunk.css"
+  },
+  {
+    "revision": "374a05391423c7d57a29",
+    "url": "/expensecalculator/static/js/2.d2d724ba.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/expensecalculator/static/js/2.d2d724ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b44509c9dc42d4322f",
+    "url": "/expensecalculator/static/js/3.426bb5c9.chunk.js"
+  },
+  {
+    "revision": "234473625504441a1c8b3815f35f5055",
+    "url": "/expensecalculator/static/js/3.426bb5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8a2a22c450b4e79107b",
+    "url": "/expensecalculator/static/js/4.9f75d876.chunk.js"
+  },
+  {
+    "revision": "14834e00d769aab4bb397241721f13c2",
+    "url": "/expensecalculator/static/js/4.9f75d876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3562c5545a174a9ec27",
+    "url": "/expensecalculator/static/js/5.d9fc3057.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/expensecalculator/static/js/5.d9fc3057.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3813435a184127f1a509",
+    "url": "/expensecalculator/static/js/main.ee705687.chunk.js"
+  },
+  {
+    "revision": "e8c77fbdc80845e5d9cd",
+    "url": "/expensecalculator/static/js/runtime-main.4680ca7f.js"
+  }
+]);
